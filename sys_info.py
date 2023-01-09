98e6bcf2ff6d373355535106bf6a0410e0598295
emrahburak <gemrahburak@gmail.com>
2023-01-09 15:41:40 +0300

    added uuid

---
+++ sys_info.py
@@ -0,0 +1,12 @@
+import subprocess
+
+# traverse the info
+Id = subprocess.check_output(['systeminfo']).decode('unicode-escape').split('\n')
+new = []
+
+# arrange the string into clear info
+for item in Id:
+	new.append(str(item.split("\r")[:-1]))
+for i in new:
+	print(i[2:-2])
+
